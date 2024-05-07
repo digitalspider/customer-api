83b17bb3b7e683e1922318414d5152ee9a48c830
Development Team <dev@digitalspider.com.au>
2024-05-07 21:20:52 +1000

    Updated endpoints

--- src/common/config.ts
+++ src/common/config.ts
@@ -4,7 +4,7 @@ export const { SECRET_MANAGER_NAME = `secrets-${AWSENV}` } = process.env;
 
 export const CUSTOMER_ENDPOINT =
   AWSENV === 'prod'
-    ? 'https://customer-api.digitalspider.com.au/v1'
-    : `https://customer-api.${AWSENV}.digitalspider.com.au/v1`;
+    ? 'https://api.digitalspider.com.au/v1'
+    : `https://api-${AWSENV}.digitalspider.com.au/v1`;
 
 export const AUTH_ENDPOINT = CUSTOMER_ENDPOINT;
